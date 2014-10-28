ba87bda31d5f945806915e77108e373dc23c0302
Convery <convery10122@gmail.com>
2014-10-28 02:30:14 +0100

    Initial.

---
+++ Logging.cs
@@ -0,0 +1,31 @@
+﻿/*
+	This project is licensed under the GPL 2.0 license. Please respect that.
+
+	Initial author: (https://github.com/)Convery
+	Started: 2014-10-28
+	Notes:
+		This logging class is based on NoFates logging class from a few years ago.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamServer
+{
+    // Severity.
+    public enum LogLevel
+    {
+        None = 0,
+        Debug = 1,
+        Info = 2,
+        Warning = 4,
+        Error = 8,
+        Data = 16,
+        All = 31
+    }
+
+    class Logging
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+	This project is licensed under the GPL 2.0 license. Please respect that.
+
+	Initial author: (https://github.com/)Convery
+	Started: 2014-10-28
+	Notes:
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
