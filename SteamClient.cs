4717590cad1d689d501941b102c85886ac852e25
Convery <convery10122@gmail.com>
2014-10-28 03:44:27 +0100

    Added messaging.

---
+++ SteamClient.cs
@@ -0,0 +1,78 @@
+﻿/*
+	This project is licensed under the GPL 2.0 license. Please respect that.
+
+	Initial author: (https://github.com/)Convery
+	Started: 2014-10-28
+	Notes:
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SteamServer
+{
+    class SteamClient
+    {
+        // Identifiers.
+        public UInt32 ClientID; // Index for arrays.
+        public UInt64 XUID;     // Ingame identifier.
+        public UInt64 HWID;     // Hardware identifier.
+
+        // Networking.
+        public Socket ClientSocket;
+        public static Int32 BufferSize = 2048;          // A client should generally send less than 1024 bytes of data per packet.
+        public Byte[] Buffer = new Byte[BufferSize];    // The data sent will be stored here.
+        public DateTime LastPacket = DateTime.Now;      // We need to drop the client if it times out.
+        public IPAddress GetIP()
+        {
+            try
+            {
+                return (ClientSocket.RemoteEndPoint as IPEndPoint).Address;
+            }
+            catch (SocketException)
+            {
+                return null;
+            }
+        }
+
+        // Client-client communication.
+        public Queue<SteamMessage> MessageQueue = new Queue<SteamMessage>();
+        public bool EnqueueMessage(SteamMessage Message)
+        {
+            try
+            {
+                // Impose a 5 sec delay on messages from the same XUID.
+                if (Math.Floor((DateTime.Now - MessageQueue.Last(item => item.IM.FromXUID == Message.IM.FromXUID).Timestamp).TotalSeconds) < 5)
+                    return false;
+
+                MessageQueue.Enqueue(Message);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Warning(e.Message);
+                return false;
+            }
+        }
+        public bool EnqueueMessage(UInt64 ToXUID, Byte[] Message)
+        {
+            SteamMessage NewMessage = new SteamMessage();
+
+            NewMessage.Timestamp = DateTime.Now;
+            NewMessage.IM.TimeStamp = (UInt32)DateTime.Now.ToFileTime();
+            NewMessage.IM.FromXUID = ToXUID;
+            NewMessage.IM.ToXUID = XUID;
+            NewMessage.IM.Message = Message;
+
+            return EnqueueMessage(NewMessage);
+        }
+
+
+
+
+    }
+}
---
+++ SteamMessage.cs
@@ -0,0 +1,68 @@
+﻿/*
+	This project is licensed under the GPL 2.0 license. Please respect that.
+
+	Initial author: (https://github.com/)Convery
+	Started: 2014-10-28
+	Notes:
+        The server does not check the contents of the packet.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamServer
+{
+    // Standard message format.
+    struct InstantMessage
+    {
+        public UInt64 FromXUID;
+        public UInt64 ToXUID;
+        public UInt32 TimeStamp;
+
+        public Byte[] Message;
+    }
+
+    // Client-client or server-client communication.
+    class SteamMessage
+    {
+        public InstantMessage IM;
+        public DateTime Timestamp;
+
+        public bool Serialize(ref redBuffer Buffer)
+        {
+            try
+            {
+                Buffer.ReadUInt64(ref IM.FromXUID);
+                Buffer.ReadUInt64(ref IM.ToXUID);
+                Buffer.ReadUInt32(ref IM.TimeStamp);
+                Buffer.ReadBlob(ref IM.Message);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+        public bool Deserialize(ref redBuffer Buffer)
+        {
+            try
+            {
+                Buffer.WriteUInt64(IM.FromXUID);
+                Buffer.WriteUInt64(IM.ToXUID);
+                Buffer.WriteUInt32(IM.TimeStamp);
+                Buffer.WriteBlob(IM.Message);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
